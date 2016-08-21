056611cbbb56b340d6c348db875406f07e88de11
Remco <caseda08@hotmail.com>
2016-08-22 00:09:31 +0200

    update to Octan Remote
    Add functionality to the Octan Remote, now waiting for answer to trigger
    flows

--- drivers/OCTAN/driver.js
+++ drivers/OCTAN/driver.js
@@ -3,26 +3,39 @@
 const path			= require('path');
 const ZwaveDriver	= require('homey-zwavedriver');
 
-// http://www.pepper1.net/zwavedb/device/766
+// http://www.pepper1.net/zwavedb/device/694
 
 module.exports = new ZwaveDriver( path.basename(__dirname), {
-	//"debug": true,
 	capabilities: {
-		
+		'measure_battery': {
+			'command_class'				: 'COMMAND_CLASS_BATTERY',
+			'command_get'				: 'BATTERY_GET',
+			'command_report'			: 'BATTERY_REPORT',
+			'command_report_parser'		: function( report ) {
+				if( report['Battery Level'] === "battery low warning" ) {
+					return 1;
+				}
+				return report['Battery Level (Raw)'][0];
+			}
+		}
+	},
+	settings: {
+		"led": {
+			"index": 8,
+			"size": 1
+		}
 	}
 });
+
 module.exports.on('initNode', function( token ){
 	var node = module.exports.nodes[ token ];
-	node.instance.CommandClass['COMMAND_CLASS_CENTRAL_SCENE'].on('report', function( command, report ){
-		console.log( report );
-	});
-	/*if( node ) {
-		node.instance.CommandClass['COMMAND_CLASS_BASIC'].on('report', function( command, report ){
-			if( report['Value'] <= 0 || report['Value'] >= 255 ) {
-				Homey.manager('flow').triggerDevice('FGD-212_s2', null, null, node.device_data);
-			} else {
-				Homey.manager('flow').triggerDevice('FGD-212_long_s2', null, null, node.device_data);
-			}
+	if( node ) {
+		node.instance.CommandClass['COMMAND_CLASS_CENTRAL_SCENE'].on('report', function( command, report ){
+			var remote_value = {
+				"button": report['Scene Number'],
+				"scene": report.Properties1['Key Attributes']
+			};
+			Homey.manager('flow').triggerDevice('octan_remote', null, remote_value, node.device_data);
 		});
-	}*/
+	}
 });

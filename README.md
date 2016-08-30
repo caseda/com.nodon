0c35a475a43d6db0a46ceed16bfec40c9bd67627
Remco <caseda08@hotmail.com>
2016-08-31 00:38:49 +0200

    update octan driver name, easier consistancy

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ This app adds support for NodOn devices in Homey.
 ## Supported Devices:
 * Octan Remote - Scene triggers for all 4 buttons (Single Click, Double Click, Long Press, Long Press Released).
 * Soft Remote - Scene triggers for all 4 buttons (Single Click, Double Click, Long Press, Long Press Released).
+
+## Supported Languages:
+* English
+* Dutch (Nederlands)
---
+++ drivers/octan_remote/driver.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const path			= require('path');
+const ZwaveDriver	= require('homey-zwavedriver');
+
+// http://www.pepper1.net/zwavedb/device/694
+
+module.exports = new ZwaveDriver( path.basename(__dirname), {
+	capabilities: {
+		'measure_battery': {
+			'command_class'				: 'COMMAND_CLASS_BATTERY',
+			'command_get'				: 'BATTERY_GET',
+			'command_report'			: 'BATTERY_REPORT',
+			'command_report_parser'		: function( report ) {
+				if( report['Battery Level'] === "battery low warning" ) {
+					return 1;
+				}
+				return report['Battery Level (Raw)'][0];
+			}
+		}
+	},
+	settings: {
+		"led": {
+			"index": 8,
+			"size": 1
+		}
+	}
+});
+
+module.exports.on('initNode', function( token ){
+	var node = module.exports.nodes[ token ];
+	if( node ) {
+		node.instance.CommandClass['COMMAND_CLASS_CENTRAL_SCENE'].on('report', function( command, report ){
+			if( command.name === 'CENTRAL_SCENE_NOTIFICATION' ) {
+				var remote_value = {
+					"button": report['Scene Number'].toString(),
+					"scene": report.Properties1['Key Attributes']
+				};
+				Homey.manager('flow').triggerDevice('octan_remote', null, remote_value, node.device_data);
+			}
+		});
+	}
+});
+
+Homey.manager('flow').on('trigger.octan_remote', function( callback, args, state ) {
+	if(state.button === args.button && state.scene === args.scene) {
+		callback(null, true);
+		return;
+	}
+	callback(null, false);
+});

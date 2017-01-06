03bc4e164db5055a401cf815955287e9991f182a
Remco <caseda08@hotmail.com>
2017-01-06 16:27:26 +0100

    update app to 1.0.3

--- README.md
+++ README.md
@@ -18,6 +18,13 @@ This app adds support for NodOn devices in Homey.
 * Dutch (Nederlands)
 
 ## Change Log:
+**1.0.3**
+Make app ES6 (ESLint) Compliant  
+Update Z-wave driver to latest (1.1.2)  
+Add wake-up interval (86400 seconds, once a day)  
+Added the funtion to Get battery level on wake-up  
+Added Association Group hints
+
 **1.0.2**
 Update z-wave driver to latest (1.0.2)  
 
--- drivers/octan_remote/driver.js
+++ drivers/octan_remote/driver.js
@@ -1,21 +1,25 @@
-"use strict";
+'use strict';
 
-const path			= require('path');
-const ZwaveDriver	= require('homey-zwavedriver');
+const path = require('path');
+const ZwaveDriver = require('homey-zwavedriver');
 
-// http://www.pepper1.net/zwavedb/device/694
+// http://nodon.fr/support/NodOn_OctanRemote_ZWave_UserGuide_EN.pdf
 
-module.exports = new ZwaveDriver( path.basename(__dirname), {
+module.exports = new ZwaveDriver(path.basename(__dirname), {
 	capabilities: {
 		'measure_battery': {
-			'command_class'				: 'COMMAND_CLASS_BATTERY',
-			'command_get'				: 'BATTERY_GET',
-			'command_report'			: 'BATTERY_REPORT',
-			'command_report_parser'		: function( report ) {
-				if( report['Battery Level'] === "battery low warning" ) {
+			'getOnWakeUp': true,
+			'command_class': 'COMMAND_CLASS_BATTERY',
+			'command_get': 'BATTERY_GET',
+			'command_report': 'BATTERY_REPORT',
+			'command_report_parser': report => {
+				if (report['Battery Level'] === 'battery low warning')
 					return 1;
-				}
-				return report['Battery Level (Raw)'][0];
+					
+				if (report.hasOwnProperty('Battery Level (Raw)'))
+					return report['Battery Level (Raw)'][0];
+				
+				return null;
 			}
 		}
 	},
@@ -27,25 +31,40 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 	}
 });
 
-module.exports.on('initNode', function( token ){
-	var node = module.exports.nodes[ token ];
-	if( node ) {
-		node.instance.CommandClass['COMMAND_CLASS_CENTRAL_SCENE'].on('report', function( command, report ){
-			if( command.name === 'CENTRAL_SCENE_NOTIFICATION' ) {
-				var remote_value = {
-					"button": report['Scene Number'].toString(),
-					"scene": report.Properties1['Key Attributes']
-				};
-				Homey.manager('flow').triggerDevice('octan_remote', null, remote_value, node.device_data);
+module.exports.on('initNode', token => {
+	const node = module.exports.nodes[token];
+	
+	if (node) {
+		node.instance.CommandClass['COMMAND_CLASS_CENTRAL_SCENE'].on('report', (command, report) => {
+			
+			if (command.name === 'CENTRAL_SCENE_NOTIFICATION') {
+				
+				if (report &&
+				report.hasOwnProperty("Scene Number") &&
+				report.hasOwnProperty("Key Attributes")) {
+					
+					const remote_value = {
+						"button": report['Scene Number'].toString(),
+						"scene": report.Properties1['Key Attributes']
+					};
+					
+					Homey.manager('flow').triggerDevice('octan_remote', null, remote_value, node.device_data);
+				}
 			}
 		});
 	}
 });
 
-Homey.manager('flow').on('trigger.octan_remote', function( callback, args, state ) {
-	if(state.button === args.button && state.scene === args.scene) {
-		callback(null, true);
-		return;
-	}
+Homey.manager('flow').on('trigger.octan_remote', (callback, args, state) => {
+	
+	if(state &&
+		state.hasOwnProperty("button") &&
+		state.hasOwnProperty("scene") &&
+		args.hasOwnProperty("button") &&
+		args.hasOwnProperty("scene") &&
+		state.button === args.button &&
+		state.scene === args.scene)
+			return callback(null, true);
+	
 	callback(null, false);
 });
